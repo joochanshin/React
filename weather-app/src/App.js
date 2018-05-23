3b9babeb26cc289a745acc38f6bf2b469f4c1b54
JooChan <14jshin@gmail.com>
2018-05-23 06:30:44 -0400

    added Weather and Form

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
 class App extends React.Component /* App is extending from this object*/{
 	render()/*Returns JSX*/ {
 		return(
 			<div>
-				
+				<Titles />
+				<Form />
+				<Weather />
 			</div>
 		);
 	}
--- weather-app/src/Components/Titles.js
+++ weather-app/src/Components/Titles.js
@@ -9,4 +9,6 @@ class Titles extends React.Component{
 			</div>
 		);
 	}
-}
+};
+
+export default Titles;
